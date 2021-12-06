eac99eb4d29e237ad9eea5436496a82e603ccc2e
nguyennguyen1112000 <nguyennguyen1112000@gmail.com>
2021-12-06 12:57:37 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="form-body">
+      <div className="row">
+        <div className="form-holder">
+          <div className="form-content">
+            <div className="form-items">
+                <div className="form-button">
+                  <button id="submit" type="submit" className="ibtn">
+                    Login with zoom
+                  </button>{" "}
+                </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
